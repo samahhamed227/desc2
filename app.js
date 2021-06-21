e8094d17af37d24726087faa09cce4a13f77add0
samahhamed227 <samahhamed227@gmail.com>
2021-06-21 16:45:47 +0300

    kk

---
+++ app.js
@@ -0,0 +1,113 @@
+
+  
+let user1 = prompt(' Is my age 25?');
+
+    if (user1==="yes") {
+        alert ("True "+user1);
+
+        console.log("yes my age is "+user1);
+        } 
+
+      else {
+            alert (user1+"false my age is 25");
+console.log(user1+"false my age is 25");
+        }
+     
+
+
+  
+let user2 = prompt(' Am i Plestanian?');
+switch(user2.toLowerCase()){
+    
+    case "yes" :
+     alert (user2+" Iam  pelstanian" );
+        console.log("Iam  pelstanian "+user2);
+    break;
+
+    
+    
+    default:
+
+alert (user2+"false Iam pelstanian ");
+console.log(user2+"false Iam pelstanian");
+break;
+
+}
+  
+let user3 = prompt('Is my name samah?');
+switch(user3.toLowerCase()){
+    
+    case "yes" :
+    alert ("my name is samah  "+user3 );
+    console.log(user3+""+" my name is  samah");
+    break;
+
+default:
+
+alert (user3+"false my name is samah ");
+console.log(user3+"false my name is samah");
+break;
+
+}
+
+
+
+
+ let user4 = prompt('Is my favorite dish is kabsa? ');
+ 
+ switch(user4.toLowerCase()){
+    
+    case "yes" :
+    alert (user4 +""+" my favourit dish is kabsa ");
+    console.log(user4 +""+" my favourit dish is kabsa ");
+
+    break;
+default:
+
+alert (user4+"false my favourit dish is kabsa");
+console.log(user4+""+"my favourit dish is kabsa");
+break;
+
+}
+
+
+let user5 = prompt('Did you enjoy with me? ');
+ 
+ switch(user5.toLowerCase()){
+    
+    case "yes" :
+    alert (user5 +""+" ohh great");
+    console.log(user5+""+" you are welcome ");
+
+    break;
+default:
+
+alert (user5+"why??? ");
+console.log(user5+""+"why???");
+break;
+
+}
+
+let user6 = prompt('what\'s you\'r name? ');
+alert("welcome"+" "+user6);
+
+document.write("welcome"+user6);
+
+
+/// const
+
+ const ID = '1236789';
+ console.log(ID);
+
+
+
+
+
+
+
+
+
+
+
+
+
